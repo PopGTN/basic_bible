7d509e9cf1bb9a4a37f5744c04555a2c6d1af42b
Joshua Mckenna <36484731+PopGTN@users.noreply.github.com>
2025-09-09 00:08:19 -0300

    Just playing around and learning

--- lib/src/features/home/about_page.dart
+++ lib/src/features/home/about_page.dart
@@ -9,9 +9,31 @@ class AboutPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('About Page')),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () => context.go('/'),
-          child: const Text('Back to Home'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('This is the about page'),
+            Text.rich(
+              TextSpan(
+                text: 'Go to ',
+                children: [
+                  TextSpan(
+                    text: 'Flutter',
+                    style: TextStyle(color: Colors.blue, decoration: TextDecoration.underline),
+                    // Recognizer can be added for tap events if needed
+                  ),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                foregroundColor: Colors.white,
+              ) ,
+              onPressed: () => context.go('/'),
+              child: const Text('Back to Home'),
+            ),
+          ],
         ),
       ),
     );
