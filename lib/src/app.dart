8869ca9426d64146ce905d1399006c9f9cc35e86
Joshua Mckenna <36484731+PopGTN@users.noreply.github.com>
2025-09-08 23:57:48 -0300

    Removed Commented out code in app.dart

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -8,33 +8,6 @@ import 'features/home/home_page.dart';
 import 'features/home/about_page.dart';
 
 
-// class MyApp extends StatelessWidget {
-//   MyApp({super.key});
-
-//   // Define routes with go_router
-//   final GoRouter _router = GoRouter(
-//     routes: [
-//       GoRoute(
-//         path: '/',
-//         builder: (context, state) => const HomeScreen(),
-//       ),
-
-//     ],
-//   );
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp.router(
-//       title: 'My Flutter App',
-//       debugShowCheckedModeBanner: false,
-//       theme: ThemeData(
-//           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-//         // primarySwatch: Colors.blue,
-//       ),
-//       routerConfig: _router,
-//     );
-//   }
-// }
 
 
 class MyApp extends StatelessWidget {
@@ -76,31 +49,3 @@ class MyAppState extends ChangeNotifier {
 }
 
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final GoRouter router = GoRouter(
-//       routes: [
-//         GoRoute(
-//           path: '/',
-//           builder: (context, state) => const HomePage(),
-//         ),
-//         GoRoute(
-//           path: '/about',
-//           builder: (context, state) => const AboutPage(),
-//         ),
-//       ],
-//     );
-
-//     return MaterialApp.router(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Bible App Test',
-//       theme: ThemeData(
-//         primarySwatch: Colors.indigo,
-//       ),
-//       routerConfig: router,
-//     );
-//   }
-// }
