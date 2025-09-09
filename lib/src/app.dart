bc894492c9d67d9e736e9c8dc56afb84ba4760cf
Joshua Mckenna <36484731+PopGTN@users.noreply.github.com>
2025-09-08 23:54:58 -0300

    Made  a basic Router
    Made  a basic Router Not sure if this is a good or not of doing this

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,8 +1,11 @@
-import 'package:basic_bible/src/screens/HomeScreen.dart';
-import 'package:basic_bible/src/screens/MyHomePage.dart';
+// import 'package:basic_bible/src/screens/HomeScreen.dart';
+// import 'package:basic_bible/src/screens/MyHomePage.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
+
+import 'features/home/home_page.dart';
+import 'features/home/about_page.dart';
 
 
 // class MyApp extends StatelessWidget {
@@ -32,19 +35,37 @@ import 'package:provider/provider.dart';
 //     );
 //   }
 // }
+
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+        final GoRouter router = GoRouter(
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state) => const HomePage(),
+        ),
+        GoRoute(
+          path: '/about',
+          builder: (context, state) => const AboutPage(),
+        ),
+      ],
+    );
+    
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Bible App',
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        title: 'Bible App Test',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          primarySwatch: Colors.indigo,
+          // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+
         ),
-        home: MyHomePage(),
+        routerConfig: router,
       ),
     );
   }
@@ -53,3 +74,33 @@ class MyApp extends StatelessWidget {
 class MyAppState extends ChangeNotifier {
   
 }
+
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final GoRouter router = GoRouter(
+//       routes: [
+//         GoRoute(
+//           path: '/',
+//           builder: (context, state) => const HomePage(),
+//         ),
+//         GoRoute(
+//           path: '/about',
+//           builder: (context, state) => const AboutPage(),
+//         ),
+//       ],
+//     );
+
+//     return MaterialApp.router(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Bible App Test',
+//       theme: ThemeData(
+//         primarySwatch: Colors.indigo,
+//       ),
+//       routerConfig: router,
+//     );
+//   }
+// }
